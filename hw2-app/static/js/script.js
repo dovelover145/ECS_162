9da44a292056a4a0983e22a254c71cb7900b63ee
Ibrahim Siddiqui <ibwaheem12@gmail.com>
2025-04-29 18:06:01 -0700

    Unit testing the Javascript business logic

--- hw2-app/static/js/script.js
+++ hw2-app/static/js/script.js
@@ -1,10 +1,12 @@
 "use strict";
 (function() {
-    const numOfArticles = 3;
-    const minArticleLength = 250;
-    
-    window.addEventListener("load", init);
-    
+    const numOfArticles = 3; /* The number of articles on the web page */
+    const minArticleDescriptionLen = 0; /* The minimum number of characters in the article's description */
+
+    if (typeof window != "undefined") { /* Will skip over this for unit tests: https://stackoverflow.com/questions/14164505/javascript-window-is-not-defined */
+        window.addEventListener("load", init);
+    }
+
     function init() { /* Useful resource: https://stackoverflow.com/questions/4904667/html-how-do-articleIdx-insert-dynamic-date-in-webpage */
         insertDate();
         insertArticleContent();
@@ -24,50 +26,62 @@
 
     async function insertArticleContent() {
         try {
-            const apiKey = await getApiKey();
-            const baseUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-            const url = new URL(baseUrl);
-            url.search = new URLSearchParams(
-                {
-                    fq: "timesTag.location:(\"Davis\", \"Sacramento\")",
-                    sort: "newest",
-                    "api-key": `${apiKey}`
-                }
-            ).toString();
-            console.log(url.toString());
-            const response = await fetch(url);
-            if (!response.ok) {
-                throw new Error("Couldn't retrieve the article content");
+            var apiKey = await getApiKey();
+        } catch (error) {
+            throw new Error(error);
+        }
+        const baseUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+        const url = new URL(baseUrl);
+        url.search = new URLSearchParams(
+            {
+                q: "Sacramento Davis",
+                fq: "timesTag.location.contains:(\"Sacramento\", \"Davis\")",
+                sort: "best",
+                "api-key": `${apiKey}`
             }
-            const responseObject = await response.json();
-            const articles = responseObject["response"]["docs"];
-            if (articles == null) {
-                return;
-            }
-            let articlesFilled = 0;
-            let articleIdx = 0;
-            while (articlesFilled < numOfArticles && articleIdx < articles.length) {
-                if (articles[articleIdx]["abstract"].length >= minArticleLength) {
-                    if (articlesFilled == 0) {
-                        document.getElementById("main-article-header").innerText = articles[articleIdx]["headline"]["main"];
-                        if (articles[articleIdx]["multimedia"]["thumbnail"]["url"] != "") {
-                            document.querySelector("#second > .image").src = articles[articleIdx]["multimedia"]["thumbnail"]["url"];
-                            document.querySelector("#second > .image").src = articles[articleIdx]["multimedia"]["thumbnail"]["caption"];
-                        }
-                        document.querySelector("#second > .article-paragraph").innerText = articles[articleIdx]["abstract"];
-                    } else if (articlesFilled == 1) {
-                        document.querySelector("#first > .article-header").innerText = articles[articleIdx]["headline"]["main"];
-                        document.querySelector("#first > .article-paragraph").innerText = articles[articleIdx]["abstract"];
-                    } else if (articlesFilled == 2) {
-                        document.querySelector("#third > .article-header").innerText = articles[articleIdx]["headline"]["main"];
-                        document.querySelector("#third > .article-paragraph").innerText = articles[articleIdx]["abstract"];
-                    }
-                    articlesFilled++;
-                }
-                articleIdx++;
-            }
+        ).toString();
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error("Couldn't retrieve the article content");
+        }
+        try {
+            var responseObject = await response.json();
         } catch (error) {
             throw new Error("Couldn't translate the article content");
+        }
+        const articles = responseObject["response"]["docs"];
+        if (articles == null) { /* Exit */
+            return;
+        }
+        let articlesFilled = 0;
+        let articleIdx = 0;
+        while (articlesFilled < numOfArticles && articleIdx < articles.length) {
+            if (articles[articleIdx]["abstract"].length >= minArticleDescriptionLen) {
+                if (articlesFilled == 0) {
+                    document.getElementById("main-article-header").innerText = articles[articleIdx]["headline"]["main"];
+                    if (articles[articleIdx]["multimedia"]["default"]["url"] != "") {
+                        document.querySelector("#second > .image").src = articles[articleIdx]["multimedia"]["default"]["url"];
+                        document.querySelector("#second > .image").alt = articles[articleIdx]["multimedia"]["caption"];
+                    }
+                    document.querySelector("#second > .article-paragraph").innerText = articles[articleIdx]["abstract"];
+                } else if (articlesFilled == 1) {
+                    document.querySelector("#first > .article-header").innerText = articles[articleIdx]["headline"]["main"];
+                    if (articles[articleIdx]["multimedia"]["default"]["url"] != "") {
+                        document.querySelector("#first > .image").src = articles[articleIdx]["multimedia"]["default"]["url"];
+                        document.querySelector("#first > .image").alt = articles[articleIdx]["multimedia"]["caption"];
+                    }
+                    document.querySelector("#first > .article-paragraph").innerText = articles[articleIdx]["abstract"];
+                } else if (articlesFilled == 2) {
+                    document.querySelector("#third > .article-header").innerText = articles[articleIdx]["headline"]["main"];
+                    if (articles[articleIdx]["multimedia"]["default"]["url"] != "") {
+                        document.querySelector("#third > .image").src = articles[articleIdx]["multimedia"]["default"]["url"];
+                        document.querySelector("#third > .image").alt = articles[articleIdx]["multimedia"]["caption"];
+                    }
+                    document.querySelector("#third > .article-paragraph").innerText = articles[articleIdx]["abstract"];
+                }
+                articlesFilled++;
+            }
+            articleIdx++;
         }
     }
 
@@ -77,10 +91,17 @@
             throw new Error("Couldn't retrieve the API key");
         }
         try {
-            const responseObject = await response.json();
-            return responseObject.apiKey;
+            var responseObject = await response.json(); /* let and const are not visible outside of the try block; var is function-scoped, so it is */
         } catch (error) {
             throw new Error("Couldn't translate the received API key");
         }
+        return responseObject.apiKey;
+    }
+
+    if (typeof module != "undefined") { /* For unit tests */
+        module.exports = {
+            getApiKey,
+            insertArticleContent
+        };
     }
 })();
---
+++ hw2-app/tests/script.test.js
@@ -0,0 +1,86 @@
+const {getApiKey, insertArticleContent} = require("../static/js/script.js")
+
+describe("Testing getApiKey()", () => {
+    beforeEach(() => {
+        global.fetch = jest.fn();
+    });
+
+    afterEach(() => {
+        jest.resetAllMocks();
+    });
+
+    test("Successful retrieval of the API key", () => {
+        global.fetch.mockImplementation(async () => {
+            return ({ok: true, json: async () => ({apiKey: "1234567890"})});
+        });
+        expect(getApiKey()).resolves.toEqual("1234567890");
+    });
+    
+    test("Couldn't retrieve the API key", () => {
+        global.fetch.mockImplementation(async () => {
+            return ({ok: false, json: async () => ({apiKey: "1234567890"})});
+        });
+        expect(getApiKey()).rejects.toThrow("Couldn't retrieve the API key"); /* Use toThrow() for synchronous functions and rejects.toThrow() for asynchronous functions * */
+    });
+
+    test("Couldn't translate the received API key", () => {
+        global.fetch.mockImplementation(async () => {
+            return ({ok: true}); /* No json field */
+        });
+        expect(getApiKey()).rejects.toThrow("Couldn't translate the received API key"); /* Use toThrow() for synchronous functions and rejects.toThrow() for asynchronous functions * */
+    });
+});
+
+describe("Testing insertArticleContent()", () => {
+    beforeEach(() => {
+        global.fetch = jest.fn();
+    });
+
+    afterEach(() => {
+        jest.resetAllMocks();
+    });
+
+    test("Couldn't retrieve the API key", () => {
+        global.fetch.mockImplementation(async (url) => {
+            if (url == "/api/key") {
+                return {ok: false, json: async () => ({apiKey: "1234567890"})};
+            } else {
+                return ({ok: false, json: async () => ({})});
+            }
+        });
+        expect(insertArticleContent()).rejects.toThrow("Couldn't retrieve the API key");
+    });
+
+    test("Couldn't translate the received API key", () => {
+        global.fetch.mockImplementation(async (url) => {
+            if (url == "/api/key") {
+                return ({ok: true});
+            } else {
+                return ({ok: false, json: async () => ({})});
+            }
+        });
+        expect(insertArticleContent()).rejects.toThrow("Couldn't translate the received API key");
+    });
+    
+    test("Couldn't retrieve the article content", () => {
+        global.fetch.mockImplementation(async (url) => {
+            if (url == "/api/key") {
+                return {ok: true, json: async () => ({apiKey: "1234567890"})};
+            } else {
+                return ({ok: false, json: async () => ({})});
+            }
+        });
+        expect(insertArticleContent()).rejects.toThrow("Couldn't retrieve the article content");
+    });
+
+    test("Couldn't translate the article content", () => {
+        global.fetch.mockImplementation(async (url) => {
+            if (url == "/api/key") {
+                return {ok: true, json: async () => ({apiKey: "1234567890"})};
+            } else {
+                return ({ok: true});
+            }
+        });
+        expect(insertArticleContent()).rejects.toThrow("Couldn't translate the article content");
+    });
+});
